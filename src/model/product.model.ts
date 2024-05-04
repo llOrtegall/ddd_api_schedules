ff3193112f5cc3b192b70d64c9415f95bad5e5f8
llOrtegall <IvanOrtega_97@hotmail.com>
2024-05-04 10:39:31 -0500

    created product and find All

--- src/model/product.model.ts
+++ src/model/product.model.ts
@@ -1,25 +1,26 @@
-import { Sequelize } from "sequelize";
-import 'dotenv/config';
+import { Model, DataTypes } from "sequelize";
+import { sequelize } from '../connections/mysql'
 
-const db_name = process.env.DB_NAME as string
-const db_user = process.env.DB_USER as string
-const db_password = process.env.DB_PASS as string
-const db_port = parseInt(process.env.DB_PORT as string)
-const db_host = process.env.DB_HOST as string
+class Product extends Model {}
 
-const sequelize = new Sequelize(db_name, db_user, db_password, {
-  host: db_host,
-  dialect: "mysql",
-  port: db_port
-})
-
-async function testConnection(){
-  try {
-    await sequelize.authenticate()
-    console.log('Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
+Product.init({
+  product_id: {
+    type: DataTypes.CHAR(36),
+    defaultValue: DataTypes.UUIDV4,
+    primaryKey: true
+  },
+  product_name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  price: {
+    type: DataTypes.FLOAT(10, 2),
+    allowNull: false
+  },
+  is_stock:{
+    type: DataTypes.BOOLEAN,
   }
-}
+}, {sequelize, modelName: 'product'})
 
-testConnection()
+
+export { Product }
--- src/router/product.router.ts
+++ src/router/product.router.ts
@@ -1,24 +1,37 @@
-import { Router } from "express";
+import { Request, Response, Router } from "express";
+import { Product } from '../model/product.model'
+import { faker } from "@faker-js/faker"; 
 
 const productsRouter = Router();
+
+productsRouter.get("/products", async (req: Request, res: Response) => {
+  const selectProducts = await Product.findAll()
+  if (!selectProducts) {
+    return res.status(404).json({ message: 'Products Not Found' })
+  }
+  return res.status(200).json({ message: 'List of Products', data: selectProducts })
+});
 
 productsRouter.get("/products", (req, res) => {
   res.send("products route");
 });
 
-productsRouter.get("/product", (req, res) => {
+productsRouter.post("/products", async (req: Request, res: Response) => {
+  await Product.sync();
+  const createProduct = await Product.create({
+    product_name: faker.commerce.productName(),
+    price: faker.commerce.price(),
+    is_stock: faker.datatype.boolean()
+  })
+
+  return res.status(201).json({ message: 'Product Created Succesfield', data: createProduct })
+});
+
+productsRouter.patch("/products", (req, res) => {
   res.send("products route");
 });
 
-productsRouter.post("/product", (req, res) => {
-  res.send("products route");
-});
-
-productsRouter.patch("/product", (req, res) => {
-  res.send("products route");
-});
-
-productsRouter.delete("/product", (req, res) => {
+productsRouter.delete("/products", (req, res) => {
   res.send("products route");
 });
 
