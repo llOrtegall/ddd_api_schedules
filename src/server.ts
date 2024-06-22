5709d8e9c803385c40512364fa72647a49421668
llOrtegall <IvanOrtega_97@hotmail.com>
2024-06-22 08:59:03 -0500

    inicializar proyecto

--- src/server.ts
+++
@@ -1,8 +0,0 @@
-import app from './app/app';
-import 'dotenv/config';
-
-const PORT = process.env.PORT || 3002;
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
--- src/types/Product.d.ts
+++
@@ -1,8 +0,0 @@
-export interface Product {
-  id: string;
-  name: string;
-  price: number;
-  is_stock: boolean;
-  createdAt: Date;
-  updatedAt: Date;
-}
