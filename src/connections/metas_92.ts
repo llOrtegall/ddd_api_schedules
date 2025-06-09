67e33e513553644d1372e112a3c722b112c8bdd8
llOrtegall <IvanOrtega_97@hotmail.com>
2025-06-09 09:32:14 -0500

    delete files

--- src/connections/metas_92.ts
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from 'sequelize'
-import 'dotenv/config'
-
-const metas92 = new Sequelize('database', 'username', 'password', {
-  host: 'localhost',
-  dialect: 'mariadb'
-})
-
-export { metas92 }
